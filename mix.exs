bfca5a64f85ab973a35b1af2f34422fd395e8bd9
preciz <barna.kovacs@tutanota.com>
2024-11-19 18:53:21 +0100

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule CommonCrawl.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github "https://github.com/preciz/common_crawl"
+
   def project do
     [
       app: :common_crawl,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      package: package(),
+      description: "Common Crawl API and WARC file parser",
+
+      # Docs
+      name: "CommonCrawl",
+      docs: docs()
     ]
   end
 
@@ -23,6 +34,22 @@ defmodule CommonCrawl.MixProject do
     [
       {:req, "~> 0.5"},
       {:jason, "~> 1.4"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Barna Kovacs"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Tmp",
+      source_ref: "v#{@version}",
+      source_url: @github
     ]
   end
 end
