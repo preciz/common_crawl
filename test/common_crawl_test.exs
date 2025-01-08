08d34bde78cc460343e15a86bdff049db806f490
preciz <barna.kovacs@tutanota.com>
2025-01-08 21:45:16 +0100

    Add tests

--- test/common_crawl_test.exs
+++ test/common_crawl_test.exs
@@ -22,4 +22,21 @@ defmodule CommonCrawlTest do
   test "updates collinfo" do
     assert :ok = CommonCrawl.update_collinfo!()
   end
+
+  @tag :integration
+  test "gets latest data for existing URL" do
+    url = "https://example.com"
+    assert {:ok, result} = CommonCrawl.get_latest_for_url(url)
+
+    assert is_map(result)
+    assert is_binary(result.warc)
+    assert is_binary(result.headers)
+    assert is_binary(result.response)
+  end
+
+  @tag :integration
+  test "returns error for non-existent URL" do
+    url = "https://this-definitely-does-not-exist-12345.com"
+    assert {:error, :not_found} = CommonCrawl.get_latest_for_url(url)
+  end
 end
